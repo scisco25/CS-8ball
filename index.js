06280874f737a7eb7f7a264563cb3dd99db7f797
scisco25 <92340877+scisco25@users.noreply.github.com>
2023-10-19 08:55:33 -0600

    comments

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let answerout = document.getElementById('answer');
 
 function ask() {
     console.log('clicked');
+    //get user input
     let question = document.getElementById('input').value.toLowerCase();
     let answer = "";
     console.log(question);
@@ -15,9 +16,13 @@ function ask() {
         if(question == "does the magic eight ball work?") {answer = "Of course.";}
         else if(question == "is javascript awesome?") {answer = "Its functional."}
         else if(question == "") {answer = "Please ask a question..."}
+        //send any user questioning the 8 ball to the void
         else if(question == "your fake!") {open("about:blank","_self")}
+        //random responses
         else{
+            //get random
             let ran = Math.random();
+            //choose response based on random
             if(ran <= 0.2){answer = "Without a Doubt."}
             else if(ran <= 0.4){answer = "As I see it, yes."}
             else if(ran <= 0.6){answer = "Concentrate and ask again."}
@@ -25,5 +30,6 @@ function ask() {
             else if(ran <= 1){answer = "Outlook not so good."}
         }
     }
+    //set the output
     answerout.innerHTML = answer;
 }
